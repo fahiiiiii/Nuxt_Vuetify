072769a96d0fb876259b3300805b4825c666bd46
Jarin Anan <jarinananmahin03@gmail.com>
2025-08-08 09:18:08 +0600

    front end workings

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout>
+    <!-- Include the hamburger drawer -->
+    <Navbar />
+
+    <!-- Page content will appear here -->
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup>
+import LayoutNavbar from '#components'
+import Navbar from '~/components/Layout/Navbar.vue';
+</script>
